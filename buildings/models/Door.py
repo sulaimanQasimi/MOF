1ddef9c375f3a57f5678d77fe1df270442334987
sulaimanQasimi <farzadqasimy@gmail.com>
2026-05-31 21:03:35 +0430

    create building models

--- buildings/models/Door.py
+++ buildings/models/Door.py
@@ -5,5 +5,6 @@ class Door(models.Model):
     material = models.CharField(max_length=50)
     is_open=models.BooleanField(default=False)
     building=models.ForeignKey('Building', on_delete=models.CASCADE, related_name='doors')
+    
     def __str__(self):
         return self.name
---
+++ buildings/models/Room.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .Building import Building
+class Room(models.Model):   
+    room_number = models.CharField(max_length=10)
+    floor = models.IntegerField()
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='rooms')  
+    
